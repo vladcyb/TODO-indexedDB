93fff773d669feea286515866949261885ff14d9
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-01-13 01:21:09 +0300

    refactor: сделал отдельный индексовый файл для каждого компонента

---
+++ src/components/Textarea/Textarea.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from 'react';
+import { createCn } from 'bem-react-classname';
+import './style.css'
+
+
+type PropsType = React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
+  label?: string
+  error?: string
+}
+
+export const Textarea: FC<PropsType> = ({
+                                          label,
+                                          required,
+                                          error,
+                                          className,
+                                          ...all
+                                        }) => {
+
+  /* classes */
+  const cn = createCn('input', className);
+
+  return (
+    <div className={cn({ error: !!error, textarea: true })}>
+      <label className={cn('label')}>
+        {label}{required ? <span className={cn('ast')}>*</span> : null}
+      </label>
+      <textarea
+        className={cn('input')}
+        {...all}
+      />
+      <div className={cn('error')}>
+        {error}
+      </div>
+    </div>
+  );
+};
