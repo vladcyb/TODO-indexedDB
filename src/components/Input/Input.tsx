eded2d2a12cf7f2ce3c98386d08108d125675660
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-01-16 01:59:11 +0300

    fix(Input): сделал label прозрачным

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -18,19 +18,25 @@ export const Input: FC<PropsType> = (
     ...inputProps
   }) => {
 
-  /* classes */
   const cn = createCn('input', className);
 
   return (
     <div className={cn({ error: !!error })}>
       <label className={cn('label')}>
-        {label}{required ? <span className={cn('ast')}>*</span> : null}
+        {label}{required && <span className={cn('ast')}>*</span>}
       </label>
-      <input
-        className={cn('input')}
-        type={type}
-        {...inputProps}
-      />
+      <div className={cn('control')}>
+        <input
+          className={cn('input')}
+          type={type}
+          {...inputProps}
+        />
+        <fieldset className={cn('fieldset')}>
+          <legend className={cn('legend')}>
+            {label}{required && '*'}
+          </legend>
+        </fieldset>
+      </div>
       <div className={cn('error')}>
         {error}
       </div>
